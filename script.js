531cb5b53efa339ebfa3b770298b13375d841124
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2023-11-04 23:50:12 +0600

    done

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let a = getElementsById("th");
 function showDiv() {
-  if (a =="") {
-    document.getElementById("alert").style.display = "block";
-    document.getElementById("others").style.display = "none";
-  }
+  document.getElementById("alert").style.display = "block";
+  document.getElementById("others").style.display = "none";
+}
+function preV()
+{
+  document.getElementById("alert").style.display = "none";
+  document.getElementById("others").style.display = "block";
 }
